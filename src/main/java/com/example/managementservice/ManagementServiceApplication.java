7cc832147cdd6538a461b5eb747e502f8ea99c5c
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-12-07 17:13:47 +0100

    - service initialization

---
+++ src/main/java/com/example/managementservice/ManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.managementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementServiceApplication.class, args);
+    }
+
+}
