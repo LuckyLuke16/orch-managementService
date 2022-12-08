ba470399d47af448d695df0f19179cee3672cada
LuckyLuke16 <83580682+LuckyLuke16@users.noreply.github.com>
2022-12-08 14:12:33 +0100

    - added path variable

--- src/main/java/com/example/managementservice/controller/ProductOperations.java
+++ src/main/java/com/example/managementservice/controller/ProductOperations.java
@@ -3,6 +3,7 @@ package com.example.managementservice.controller;
 import com.example.managementservice.model.ItemDTO;
 import com.example.managementservice.model.ItemDetailDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public interface ProductOperations {
     @GetMapping
     List<ItemDTO> fetchAllItems();
 
-    @GetMapping
-    ItemDetailDTO fetchSingleItem();
+    @GetMapping("/{itemID}")
+    ItemDetailDTO fetchSingleItem(@PathVariable String itemID);
 
 }
